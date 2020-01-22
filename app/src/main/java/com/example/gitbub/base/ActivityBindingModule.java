1b1c2a34b1bbeace94678d3dc4eacc91b6a4a0f6
Divyansh Dwivedi <justdvnsh2208@gmail.com>
2020-01-22 14:31:46 +0530

    added activity scope

---
+++ app/src/main/java/com/example/gitbub/base/ActivityBindingModule.java
@@ -0,0 +1,24 @@
+package com.example.gitbub.base;
+
+
+import android.app.Activity;
+
+import com.example.gitbub.home.MainActivity;
+import com.example.gitbub.home.MainActivityComponent;
+
+import dagger.Binds;
+import dagger.Module;
+import dagger.android.ActivityKey;
+import dagger.android.AndroidInjector;
+import dagger.multibindings.IntoMap;
+
+@Module(subcomponents = {
+        MainActivityComponent.class
+})
+public abstract class ActivityBindingModule {
+
+    @Binds
+    @IntoMap
+    @ActivityKey(MainActivity.class)
+    abstract AndroidInjector.Factory<? extends Activity> provideMainActivityInjector(MainActivityComponent.Builder builder);
+}
--- app/src/main/java/com/example/gitbub/home/MainActivity.java
+++ app/src/main/java/com/example/gitbub/home/MainActivity.java
@@ -1,8 +1,10 @@
-package com.example.gitbub;
+package com.example.gitbub.home;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import com.example.gitbub.R;
 
 public class MainActivity extends AppCompatActivity {
 
